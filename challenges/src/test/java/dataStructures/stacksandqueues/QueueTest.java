58478da6858e7c1024415ff2683a6cb7df93fc56
Meghan Domeck <mdomeck@aol.com>
2020-09-26 17:06:43 -0400

    done

--- challenges/src/test/java/dataStructures/stacksandqueues/QueueTest.java
+++ challenges/src/test/java/dataStructures/stacksandqueues/QueueTest.java
@@ -2,8 +2,7 @@ package dataStructures.stacksandqueues;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNull;
+import static org.junit.Assert.*;
 
 public class QueueTest {
 
@@ -11,40 +10,60 @@ public class QueueTest {
     public void enqueueIntoQueue() {
         Queue queue = new Queue();
         queue.enqueue(4);
-        queue.enqueue(6);
-        assertEquals("this queue is front ", 6, queue.front);
-        assertEquals("this queue is next", 4, queue.back);
+        assertEquals("this queue is front ", 4, queue.front.value);
     }
 
     @Test
     public void enqueueMultipleValues() {
-
+        Queue queue = new Queue();
+        queue.enqueue(4);
+        queue.enqueue(6);
+        queue.enqueue(8);
+        assertEquals("this queue is front ", 4, queue.front.value);
+        assertEquals("this queue is back", 8, queue.back.value);
     }
 
     @Test
     public void dequeueOutExpectedValue() {
-
+        Queue queue = new Queue();
+        queue.enqueue(4);
+        queue.enqueue(6);
+        assertEquals("this queue is front ", 4, queue.front.value);
+        queue.dequeue();
+        assertEquals("this is the new front ",6, queue.front.value);
+        assertThrows(NullPointerException.class, () -> queue.dequeue());
     }
 
     @Test
     public void peekIntoQueueExpectedValue() {
-
+        Queue queue = new Queue();
+        queue.enqueue(4);
+        queue.enqueue(6);
+        queue.enqueue(8);
+        assertEquals("peeking at the front ", 4, queue.peek());
+        assertEquals("this queue is front ", 4, queue.front.value);
     }
 
     @Test
     public void emptyQueueAfterMultipleDequeues() {
-
+        Queue queue = new Queue();
+        queue.enqueue(4);
+        queue.enqueue(6);
+        queue.enqueue(8);
+        queue.dequeue();
+        queue.dequeue();
+        assertThrows(NullPointerException.class, () -> queue.dequeue());
     }
 
     @Test
     public void instantiateEmptyQueue() {
         Queue queue = new Queue();
-
-        assertNull("this queue is empty", queue.back);
+        assertNull("this queue is empty", queue.front);
     }
 
     @Test
     public void emptyQueueException() {
-
+        Queue queue = new Queue();
+        assertTrue("This should be empty", queue.isEmpty());
     }
 }
