38b8a43418f17b14ded57900c30a4f9e8dbad9a9
Meghan Domeck <mdomeck@aol.com>
2021-01-29 14:29:12 -0500

    working anagram

--- challenges/src/main/java/codewars/IsAnagram.java
+++ challenges/src/main/java/codewars/IsAnagram.java
@@ -5,13 +5,11 @@ import java.util.Arrays;
 public class IsAnagram {
     public static boolean isAnagram(String test, String original) {
 
-        char[] word1 = test.replaceAll("[\\s]", "").toCharArray();
-        char[] word2 = original.replaceAll("[\\s]", "").toCharArray();
+        char[] word1 = test.toLowerCase().toCharArray();
+        char[] word2 = original.toLowerCase().toCharArray();
 
         Arrays.sort(word1);
-        System.out.println(word1);
         Arrays.sort(word2);
-        System.out.println(word2);
         return Arrays.equals(word1, word2);
     }
 }
