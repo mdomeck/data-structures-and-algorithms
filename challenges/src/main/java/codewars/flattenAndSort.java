e0ef4936ab85e40ce1ccd27813c1ac1b8dcefc25
Meghan Domeck <mdomeck@aol.com>
2021-02-05 23:36:15 -0500

    working with stream

--- challenges/src/main/java/codewars/flattenAndSort.java
+++ challenges/src/main/java/codewars/flattenAndSort.java
@@ -1,15 +1,10 @@
 package codewars;
 
+import java.util.Arrays;
+
 public class flattenAndSort {
     public static int[] flattenAndSort(int[][] array) {
-        
-        int result[] = new int[array.length * array[0].length];
-        int index = 0;
-        for(int i = 0; i < array.length; i++){
-            for(int j = 0; j < array.length; j++){
-                result[index++] = array[i][j];
-            }
-        }
-        return result;
+        int[] flatArray = Arrays.stream(array).flatMapToInt(Arrays::stream).sorted().toArray();
+        return flatArray;
     }
 }
