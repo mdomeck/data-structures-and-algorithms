5db5e71d5ebdfb9b16a4d908bc3e65ea1065b050
Meghan Domeck <mdomeck@aol.com>
2021-02-16 22:16:57 -0500

    declare winner setup

---
+++ challenges/src/test/java/codewars/DeclareWinnerTest.java
@@ -0,0 +1,17 @@
+package codewars;
+
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class DeclareWinnerTest {
+    @Test
+    public void basicTests() {
+        assertEquals("Lew", DeclareWinner.declareWinner(new DeclareWinner.Fighter("Lew", 10, 2),new DeclareWinner.Fighter("Harry", 5, 4), "Lew"));
+        assertEquals("Harry", DeclareWinner.declareWinner(new DeclareWinner.Fighter("Lew", 10, 2),new DeclareWinner.Fighter("Harry", 5, 4), "Harry"));
+        assertEquals("Harald", DeclareWinner.declareWinner(new DeclareWinner.Fighter("Harald", 20, 5), new DeclareWinner.Fighter("Harry", 5, 4), "Harry"));
+        assertEquals("Harald", DeclareWinner.declareWinner(new DeclareWinner.Fighter("Harald", 20, 5), new DeclareWinner.Fighter("Harry", 5, 4), "Harald"));
+        assertEquals("Harald", DeclareWinner.declareWinner(new DeclareWinner.Fighter("Jerry", 30, 3), new DeclareWinner.Fighter("Harald", 20, 5), "Jerry"));
+        assertEquals("Harald", DeclareWinner.declareWinner(new DeclareWinner.Fighter("Jerry", 30, 3), new DeclareWinner.Fighter("Harald", 20, 5), "Harald"));
+    }
+}
