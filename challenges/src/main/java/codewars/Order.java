c37fbefc7044c706defe36f037a3ade642dd32a1
Meghan Domeck <mdomeck@aol.com>
2021-04-06 22:33:15 -0400

    done with help

--- challenges/src/main/java/codewars/Order.java
+++ challenges/src/main/java/codewars/Order.java
@@ -1,8 +1,29 @@
 package codewars;
 
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.StringTokenizer;
+
 public class Order {
     public static String order(String words) {
-        // ...
-        return words;
+        List<Character> numbers = new ArrayList<Character>(Arrays.asList('1', '2', '3', '4', '5', '6', '7', '8', '9'));
+        StringTokenizer st = new StringTokenizer(words);
+        String[] stringWords = new String[st.countTokens()];
+
+        while(st.hasMoreTokens()) {
+            String currentWord = st.nextToken();
+            for (int i = 0; i < currentWord.length(); i++) {
+                if (numbers.contains(currentWord.charAt(i))) {
+                    stringWords[numbers.indexOf(currentWord.charAt(i))] = currentWord;
+                }
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        for (String s: stringWords) {
+            sb.append(s + ' ');
+        }
+        return sb.toString().trim();
     }
 }
